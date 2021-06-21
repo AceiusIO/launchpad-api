daba8d7fecdf9e4b96a1df8feef5b2d9bb949f6d
Aceius <io@acei.us>
2021-06-21 08:54:06 -0700

    Update App.vue
    Improvements mostly

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <v-app dark="true">
+  <v-app>
     <v-card v-if="stage1">
       <v-card-title>
         <h1>Enter Code</h1>
       </v-card-title>
       <v-card-text>
-        <v-text-field label="API Key" outlined clearable v-model="keyInput"></v-text-field>
+        <v-text-field label="API Key" outlined clearable autocomplete="off" v-model="keyInput"></v-text-field>
         <v-card-actions class="justify-center">
-          <v-btn :loading="loading" :disabled="loading" color="red" class="ma-2 white--text" @click="checkAPI();" v-model="passkeyInput">
-            Launch <v-icon dark>mdi-rocket-launch</v-icon>
+          <v-btn :loading="loading" :disabled="loading" color="red" class="ma-2 white--text" @click="checkAPI();">
+            Launch <v-icon>mdi-rocket-launch</v-icon>
           </v-btn>
         </v-card-actions>
       </v-card-text>
     </v-card>
     <v-card v-if="stage2">
       <v-card-actions class="justify-center">
-        <v-btn :loading="loading" :disabled="loading" color="red" fab class="ma-2 white--text" @click="confirmCheckAPI();" v-model="passkeyInput" width="150" height="150">          <v-icon dark>mdi-rocket-launch</v-icon>
+        <v-btn :loading="loading" :disabled="loading" color="red" fab class="ma-2 white--text" @click="fire();" width="150" height="150"> <v-icon>mdi-rocket-launch</v-icon>
         </v-btn>
       </v-card-actions>
       <v-card-text>
@@ -37,6 +37,7 @@ export default {
 
   data: () => ({
     passKey: '1234',
+    keyInput: null,
     loader: null,
     loading: false,
     stage1: true,
@@ -57,6 +58,7 @@ export default {
   methods: {
     checkAPI: function () {
       this.loader = 'loading'
+      this.apiResponse = "Waiting"
       if (this.keyInput == this.passKey) {
         this.stage1 = false
         this.stage2 = true
@@ -66,16 +68,25 @@ export default {
       }
       
     },
-    confirmCheckAPI: function () {
-      this.httpGet('10.0.0.51:80/fire');
+
+    fire: function () {
+      this.loader = 'loading'
+      this.apiResponse = 'Preparing'
+      if (this.keyInput == this.passKey) {
+        this.httpGet('http://api.mojang.com/');
+      } else {
+        alert('DANGER: Authentication failed, but trigger fired!');
+      }
     },
+
     httpGet: function (Url) {
+      this.apiResponse = "Initalizing XML Request"
       const xmlHttp = new XMLHttpRequest();
+      this.apiResponse = "Requesting Data"
       xmlHttp.open("GET", Url, false);
       xmlHttp.send(null);
+      this.apiResponse = "<v-icon>mdi-checkmark</v-icon>Sucess!"
       console.log(xmlHttp.responseText);
-      this.apiResponse = "Sucess!"
-      //return xmlHttp.responseText;
     },
   }
 };
